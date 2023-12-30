a3e98a4fb0e80fc90ddb94792969fbf5146a924f
PIYUSH SINGH <piyushsingh9862@gmail.com>
2023-12-30 18:03:58 +0530

    Enhanced the visual appeal

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,35 +1,42 @@
 
 import React from 'react';
+import { Container, Typography, Grid, Card, CardContent } from '@mui/material';
+
+const cardStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  minHeight: '200px',
+};
+
+const titleStyle = {
+  fontFamily: 'Roboto, sans-serif', 
+};
 
 const ProductList = ({ data }) => {
-  console.log('Received data:', data);
-
   if (!data || data.length === 0) {
     return <p>No data available</p>;
   }
 
   return (
-    <div>
-      <h1>Product List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((product, index) => (
-            <tr key={index}>
-              <td>{product.title}</td>
-              <td>{product.price}</td>
-              <td>{product.popularity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Container>
+      <Typography variant="h3" align="center" gutterBottom style={titleStyle}>
+        Product List
+      </Typography>
+      <Grid container spacing={3}>
+        {data.map((product, index) => (
+          <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+            <Card style={cardStyle}>
+              <CardContent>
+                <Typography variant="h6">{product.title}</Typography>
+                <Typography variant="body1">Price: ${product.price}</Typography>
+                <Typography variant="body1">Popularity: {product.popularity}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
